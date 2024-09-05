0e4cdb61ad0d15004a8587cf948976e0f07f5751
AditiMahangade <aditi.a.mahangade-1@ou.edu>
2024-09-05 10:14:50 -0500

    Add project files.

---
+++ Class&FilePartTwo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class_FilePartTwo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string[] pieces)
+        {
+            InitializeComponent();
+
+
+            string[] linesofFile = File.ReadAllLines("SalesJan2009");
+
+            foreach (var line in linesofFile)
+            {
+                var pieces = line.Split(",");
+
+                Payment p = new Payment();
+
+                p.Price = Double.Parse(pieces[0]);
+                p.PaymentType = pieces[1];
+                p.Name = pieces[2];
+                p.Country = pieces[3];
+               
+
+                MasterCardLB.Items.Add(p);
+                VisaLB.Items.Add(p);
+                DinersLB.Items.Add(p);
+                AmexLB.Items.Add(p);
+
+            }
+        }
+    }
+}
---
+++ Class&FilePartTwo/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Class_FilePartTwo
+{
+    internal class Payment
+    {
+        public DateTime TransactionDate { get; set; }
+        public double Price { get; set; }
+        public string PaymentType { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        private double Sale { get; set; }
+    }
+    public string Sale()
+    {
+        return $"{Payment.ToUpper()[0]}{Name}{Price}";
+        MessageBox.Show(country);
+    }
+}
